670f9da2b2c720576131d1c3a6a37c6a8d3aaa0a
Nussaibah Raja-Schoob <nussaibah.raja.schoob@fau.de>
2020-03-10 15:03:15 +0100

    results for multi classification

--- code/04_results_multi.R
+++ code/04_results_multi.R
@@ -46,6 +46,7 @@ df$branching <-  factor(df$branching, levels=c("NB", "LB", "MB", "HB"))
 df$iucn <- as.factor(df$iucn)
 
 levels(df$iucn) <- c("EN", "EN", "LC", "NT", "VU")
+levels(df$iucn) <- c("LC", "NT", "VU", "EN")
 
 # Set up h2o session ------------------------------------------------------
 
@@ -74,10 +75,10 @@ x <- c("max_depth", "branching", "corallite", "range" )
 folder_name <- "model_multi"
 leaderboard <- read.csv(here("output", folder_name, "leaderboard.csv"), stringsAsFactors = FALSE)
 aml_leader <- h2o.loadModel(here("output", folder_name, leaderboard$model_id[1])) #load top one
+aml_leader@algorithm
 
 # * Confusion Matrix ------------------------------------------------------
 res.train <- h2o.predict(aml_leader, train)
-
 res.test <- h2o.predict(aml_leader , test)
 
 #all values lower than cutoff value will be classified as 0 (NT in this case)
@@ -196,7 +197,7 @@ p_vi <- aml_varimp %>%
         axis.text.x = element_text(family = "Roboto Mono", size = 10),
         panel.grid = element_blank()) 
 
-ggsave (here("figs", "Fig_02_var_imp.svg"), p_vi, h=4, w=8)
+p_vi
 
 # *  Partial dependence profile ---------------------------------------------------
 
@@ -251,188 +252,47 @@ pdp_plot <- function (pdp, olddata=NULL, levels=NULL, unscale=TRUE, col="darkred
 
 # plot pdp
 p1 <- pdp_plot(pdp_corallite, olddata=scale(df$corallite_ori), col = u_col[2]) + labs(x="Corallite Diameter (mm)") + 
-  coord_cartesian(xlim=c(0, 25)) + geom_hline(yintercept = opt_par_final$cutoff[win], linetype="dashed", col="darkgrey")
+  coord_cartesian(xlim=c(0, 25)) 
 
 p2 <- pdp_plot(pdp_branching, unscale = FALSE, levels=c("NB", "LB", "MB", "HB")) + scale_fill_manual(values=u_col[2:5]) + 
-  labs(x="Degree of branching")  + geom_hline(yintercept = opt_par_final$cutoff[win], linetype="dashed", col="darkgrey")
+  labs(x="Degree of branching")  
 
 p3 <- pdp_plot(pdp_range, olddata=scale(df$range_ori), col = u_col[3]) + labs(x="Range (normalised)") + 
   geom_hline(yintercept = opt_par_final$cutoff[win], linetype="dashed", col="darkgrey")
 
 p4 <- pdp_plot(pdp_max_depth, olddata=scale(df$depth_ori), col = u_col[4]) + labs(x="Maximum depth (m)") +
-  coord_cartesian(xlim=c(0, 100)) + geom_hline(yintercept = opt_par_final$cutoff[win], linetype="dashed", col="darkgrey")
+  coord_cartesian(xlim=c(0, 100)) 
 
-svg(here("figs", "Fig_03_model_metrics.svg"), w=8, h=8)
+
 p1 + p2 + p3 + p4  + plot_annotation(tag_levels = "A")
-dev.off()
 
 # DD species ------------------------------------------------
-dd <- df.corals %>% filter(iucn =="Data Deficient") %>% select (-iucn)
+dd <- df.corals %>% filter(iucn =="Data Deficient") %>% select (-iucn) 
 
 # * Prediction of threat status -------------------------------------------
 
 res <- h2o.predict(aml_leader, as.h2o(dd))
-res <- as.data.frame(res)$p1
 
 # calculate threat status based on threshold cutoff
-dd$status <- ifelse(res < opt_par_final$cutoff[win], "NT", "T")
+dd$status <- as.data.frame(res)$predict %>% as.character()
 
 #number of missing values for each species
-dd$na_count <- apply(dd, 1, function(x) sum(is.na(x)))
+dd$na_count <- apply(dd[,x], 1, function(x) sum(is.na(x)))
 dd[dd$na_count >2,]$status <- "DD"
 
 #get genus for each species
 dd$genus <- gsub( " .*$", "", dd$sp)
 
 # ** Fig. 4: Threat status by genera (no. of sp > 2) -----------------------
-dd_plot <- dd %>% filter(status != "DD") %>%
-  mutate(na_count = ifelse(na_count == 0, "complete", "incomplete")) %>%
-  group_by(genus, status, na_count) %>%
+dd %>% filter(status != "DD") %>%
+  group_by(genus, status) %>%
   tally() %>%
   group_by(genus) %>%
   mutate(n2 = sum(n),
          n3 = n/n2) %>%
   arrange(desc(n2)) %>%
-  filter(n2 > 1) %>% 
-  mutate(n3 =ifelse(status == "NT", -n3, n3),
-         status2 = paste0(status, substring(na_count, 1, 1))) %>%
-  mutate(status2 = ifelse(status2 %in% c("NTi", "Ti"), "DD", status2), 
-         status2 = factor(status2, levels=c("NTc", "Tc","DD"))) %>%
-  arrange(genus, status2)
+  filter(n2 > 1) 
 
-dd_sum <- dd_plot %>% ungroup() %>% group_by(genus, status) %>% mutate(tot=sum(n), n=sum(n3)) %>%
-  mutate(status2 = ifelse(status == "NT", "NTc", "Tc")) %>% distinct(genus, status2, n, tot)
-
-p <- ggplot(dd_plot, aes(fill=status2, y=n3, x=reorder(genus, n2))) +
-  geom_bar(stat="identity", alpha=0.5, width=0.6) +
-  geom_point(data=dd_sum , 
-             aes(x=genus, y=n, col=status2), cex=7.5, inherit.aes = FALSE) +
-  geom_text(data=dd_sum, aes(x=genus, y=n, label=tot), col="white", fontface="bold", inherit.aes = FALSE) +
-  scale_y_continuous(breaks = seq(-1,1, 0.5), labels = abs(seq(-1,1, 0.5))) +
-  scale_x_discrete(expand=expand_scale(mult=0.05))+
-  scale_color_manual(values=u_col[c(2,5)], labels=c("Not threatened", "Threatened"), guide=FALSE)+
-  scale_fill_manual(values=u_col[c(2,5,1)], labels=c("Not threatened", "Threatened", "Data-deficient"))+
-  labs(x="Genus", y="Proportion", fill="Status") +
-  coord_flip() +
-  geom_hline(yintercept = 0, col="darkgrey") +
-  theme_light(base_size = 15) +
-  theme(axis.title = element_text(size = 12, face="bold"),
-        legend.title = element_text(size = 12, face="bold"),
-        legend.text = element_text(family = "Roboto Mono", size = 10),
-        axis.text = element_text(family = "Roboto Mono", size = 10),
-        panel.grid = element_blank(), 
-        legend.position = "none",
-        plot.margin = unit(c(2, 1, 1, 1), "lines"))
-
-svg(here("figs", "Fig_04_dd_species.svg"), w=6, h=6)
-p 
-grid::grid.text("Threatened", x = unit(0.45, "npc"), y = unit(.95, "npc"), 
-                gp=gpar(family="Roboto Mono", fontface="bold"), hjust=-1.04)
-grid::grid.text("Not Threatened", x = unit(0.45, "npc"), y = unit(.95, "npc"), 
-                gp=gpar(family="Roboto Mono", fontface="bold"), hjust=0.3)
-dev.off()
-
-# * Map of DD distribution risk -------------------------------------------
-gr <- hexagrid(16, sp=TRUE) #2.5 degrees
-world <- rworldmap::getMap()
-cuts <- seq(0.0, 1, 0.2)
-map_col <- colorRampPalette(u_col[c(2,4,5)])(length(cuts)-1)
-
-#### DD only
-dd_occ <- read.csv(here("data", "2019-11-05_obis_scleractinia.csv"), stringsAsFactors = FALSE) %>%
-  filter(!between(decimalLatitude, 39, 45) & !between(decimalLongitude, -38, 30)) %>% #remove mediterranean ones
-  filter(scientificName %in% dd$sp) %>% #select only occurrences for dd species
-  distinct(scientificName, decimalLatitude, decimalLongitude) %>%
-  left_join(dd %>% dplyr::select(sp, status), by=c("scientificName" = "sp"))
-
-dd_occ$cell <- locate(gr, dd_occ[, c(3,2)])
-
-me_dd <- tapply(INDEX=dd_occ$cell, X=dd_occ$status, function (x) length(x[x == "T"])/length(x[x=="NT"| x == "T"]))
-mean(me_dd, na.rm = TRUE)
-
-
-
-##### DS only
-ds_occ <- read.csv(here("data", "2019-11-05_obis_scleractinia.csv"), stringsAsFactors = FALSE) %>%
-  filter(!between(decimalLatitude, 39, 45) & !between(decimalLongitude, -38, 30)) %>% #remove mediterranean ones
-  filter(scientificName %in% df$sp) %>% #select data sufficient species only
-  distinct(scientificName, decimalLatitude, decimalLongitude) %>%
-  left_join(df %>% dplyr::select(sp, iucn) %>% mutate(status = ifelse(iucn == 0, "NT", "T")) %>% select(-iucn), by=c("scientificName" = "sp"))
-
-ds_occ$cell <- locate(gr, ds_occ[, c(3,2)])
-
-me_ds <- tapply(INDEX=ds_occ$cell, X=ds_occ$status, function (x) length(x[x == "T"])/length(x[x=="NT"| x == "T"]))
-mean(me_ds, na.rm = TRUE)
-
-#### DS + DD
-all_occ <- rbind(ds_occ, dd_occ)
-
-all_occ$cell <- locate(gr, all_occ[, c(3,2)])
-
-me <- tapply(INDEX=all_occ$cell, X=all_occ$status, function (x) length(x[x == "T"])/length(x[x=="NT"| x == "T"]))
-mean(me, na.rm = TRUE)
-
-##### plot
-txt <- 1.2
-asp=1.2
-svg(here("figs", "Fig_05_map_dd_risk.svg"), w=6, h=6.5)
-#x11(w=6, h=6.5)
-layout(matrix(c(1,2,3,4), nrow=4, byrow=TRUE),
-       heights = c(1,1,1,0.3))
-par(mar=c(1,3,2,1))
-
-plot(NULL, xlim=c(-180, 180), ylim=c(-30,30), axes=FALSE, xlab="", ylab="",
-     asp=asp)
-
-for (i in 1:(length(cuts)-1)){
-  n <- which(me_ds >= cuts[i] & me_ds < cuts[i+1])  
-  temp <- names(n)
-  
-  plot(gr[temp], col=map_col[i], border="white", add=TRUE)
-}
-
-plot(world, col=scales::alpha("grey95", 0.8), border="grey80", add=TRUE)
-
-mtext ("A", side=3, line=0, adj=-0.04, cex=txt)
-box(col="darkgrey")
-# dd
-plot(NULL, xlim=c(-180, 180), ylim=c(-30,30), axes=FALSE, xlab="", ylab="",
-     asp=asp)
-
-for (i in 1:(length(cuts)-1)){
-  n <- which(me_dd >= cuts[i] & me_dd < cuts[i+1])  
-  temp <- names(n)
-  
-  plot(gr[temp], col=map_col[i], border="white", add=TRUE)
-}
-
-plot(world, col=scales::alpha("grey95", 0.8), border="grey80", add=TRUE)
-mtext ("B", side=3, line=0, adj=-0.04, cex=txt)
-box(col="darkgrey")
-# combined
-plot(NULL, xlim=c(-180, 180), ylim=c(-30,30), axes=FALSE, xlab="", ylab="",
-     asp=asp)
-
-for (i in 1:(length(cuts)-1)){
-  n <- which(me >= cuts[i] & me < cuts[i+1])  
-  temp <- names(n)
-  
-  plot(gr[temp], col=map_col[i], border="white", add=TRUE)
-}
-
-plot(world, col=scales::alpha("grey95", 0.8), border="grey80", add=TRUE)
-mtext ("C", side=3, line=0, adj=-0.04, cex=txt)
-box(col="darkgrey")
-#add legend
-par(xpd=TRUE)
-plot(0,0, type="n", axes=FALSE)
-cuts_labs <-cuts*100 #turn to percentage
-legend("center", paste0(cuts_labs[1:(length(cuts)-1)], "-", cuts_labs[2:length(cuts)], "%"), fill=map_col, horiz=TRUE, border=NA, bty="n", 
-       title = expression(bold("Percentage of coral species under threat")), 
-       cex=1.2)
-
-
-dev.off()
 
 # Plio-Pleistocene --------------------------------------------------------
 
@@ -455,22 +315,20 @@ pleist.df[pleist.df$branching == "",]$branching <- NA
 
 pleist.df[pleist.df$globally.extinct == "extinct" & pleist.df$regionally.extinct == "extant",]$regionally.extinct <- "extinct"
 res <- h2o.predict(aml_leader, as.h2o(pleist.df))
-res <- as.data.frame(res)$p1
+res <- as.data.frame(res)$predict
 
 # calculate threat status based on threshold cutoff
-pleist.df$status <- ifelse(res < opt_par_final$cutoff[win], "NT", "T")
+pleist.df$status <- res %>% as.character()
 
 #number of missing values for each species
-pleist.df$na_count <- apply(pleist.df, 1, function(x) sum(is.na(x)))
+pleist.df$na_count <- apply(pleist.df[,x], 1, function(x) sum(is.na(x)))
 pleist.df[pleist.df$na_count >2,]$status <- "DD"
 
 prop.table(table(pleist.df$status[pleist.df$status != "DD"]))
 
 pleist.summ <- pleist.df %>% 
   filter(status != "DD") %>%
-  mutate(status = case_when(status == "T" ~ "Threatened", 
-                            status == "NT" ~"Not Threatened"),
-         globally.extinct = tools::toTitleCase(globally.extinct),
+  mutate(globally.extinct = tools::toTitleCase(globally.extinct),
          regionally.extinct = tools::toTitleCase(regionally.extinct)) %>%
   group_by(globally.extinct, regionally.extinct, status) %>%
   tally() %>%
@@ -507,7 +365,6 @@ ggplot(pleist.summ,
         legend.position = "top",
         legend.direction = "horizontal")
 
-ggsave(here("figs", "Fig_06_pleist_predictions.svg"), w=6, h=8)
 
 ### per genus
 df.ds <- df.corals %>%
@@ -577,4 +434,3 @@ ggplot(df.comp, aes(x=fossil, y=modern, label=paste0(genus, "(", tot, "/", totm,
         legend.position = "top",
         legend.direction = "horizontal")
 
-ggsave(here("figs", "Fig_S_plio_modern_comp.svg"), w=7, h=7)
